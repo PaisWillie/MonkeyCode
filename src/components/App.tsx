fd64e80be8ad9606f93a137f567fb3e3eecc6d9d
Willie Pai <PaisWillie@gmail.com>
2024-12-12 15:53:07 -0500

    feat: add theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,48 +1,59 @@
-import axios from 'axios'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
-const API_KEY = import.meta.env.VITE_OPENAI_API_KEY
+const snippet = `def threeSum(nums):
+    nums.sort()  # Sort the array to make it easier to skip duplicates
+    result = []
 
-const openai = axios.create({
-  baseURL: 'https://api.openai.com/v1',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${API_KEY}`
-  }
-})
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
 
-export const getOpenAIResponse = async (prompt: string) => {
-  const response = await openai.post('/chat/completions', {
-    model: 'gpt-4o-mini',
-    messages: [
-      {
-        role: 'system',
-        content:
-          'Generate a code snippet based on the user prompt. Do not include other information other than the code snippet.'
-      },
-      { role: 'user', content: prompt }
-    ]
-  })
-  return response.data
-}
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+            if total == 0:
+                result.append([nums[i], nums[left], nums[right]])
+                left += 1
+                right -= 1
+
+                while left < right and nums[left] == nums[left - 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right + 1]:
+                    right -= 1
+            elif total < 0:
+                left += 1
+            else:
+                right -= 1
+
+    return result
+
+# Example usage
+nums = [-1, 0, 1, 2, -1, -4]
+print(threeSum(nums))
+`
 
 function App() {
-  const [response, setResponse] = useState('')
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [input, setInput] = useState<string>('')
+
+  const handlePreClick = () => {
+    inputRef.current?.focus()
+  }
 
   return (
-    <div>
-      <button
-        onClick={async () => {
-          const response = await getOpenAIResponse(
-            'Leetcode Java medium sliding window problem'
-          )
-          setResponse(response.choices[0].message.content)
-          console.log(response)
-        }}
-      >
-        Generate
-      </button>
-      <pre>{response}</pre>
+    <div className="flex h-screen w-screen flex-col items-center justify-between bg-[#323437] font-['Space_Mono'] text-[#646669]">
+      <header></header>
+      <main>
+        <pre onClick={handlePreClick}>{snippet}</pre>
+        <input
+          className="absolute -top-10"
+          ref={inputRef}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></input>
+        <p>Value: {input}</p>
+      </main>
+      <footer></footer>
     </div>
   )
 }
