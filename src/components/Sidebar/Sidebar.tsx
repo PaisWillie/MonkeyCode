87df02ad31aa6c922bdbb3d0773e04cec834e912
Willie Pai <PaisWillie@gmail.com>
2025-01-07 10:47:17 -0500

    feat: update metric styling

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -19,36 +19,48 @@ const Sidebar = ({
     <div className="flex flex-col">
       <div className="flex flex-col">
         <span>cpm</span>
-        <span className={clsx([cpm ? 'text-white' : 'text-slate-400'])}>
+        <span
+          className={clsx([metricStyle, cpm ? 'text-white' : 'text-slate-400'])}
+        >
           {cpm || 'TBD'}
         </span>
       </div>
       <div className="flex flex-col">
         <span>raw cpm</span>
-        <span className={clsx([cpm ? 'text-white' : 'text-slate-400'])}>
+        <span
+          className={clsx([metricStyle, cpm ? 'text-white' : 'text-slate-400'])}
+        >
           {rawCpm || 'TBD'}
         </span>
       </div>
       <div className="flex flex-col">
         <span>accuracy</span>
-        <span className={clsx([cpm ? 'text-white' : 'text-slate-400'])}>
+        <span
+          className={clsx([metricStyle, cpm ? 'text-white' : 'text-slate-400'])}
+        >
           {accuracy || 'TBD'}
         </span>
       </div>
       <div className="flex flex-col">
         <span>raw accuracy</span>
-        <span className={clsx([cpm ? 'text-white' : 'text-slate-400'])}>
+        <span
+          className={clsx([metricStyle, cpm ? 'text-white' : 'text-slate-400'])}
+        >
           {rawAccuracy || 'TBD'}
         </span>
       </div>
       <div className="flex flex-col">
         <span>time</span>
-        <span className={clsx([cpm ? 'text-white' : 'text-slate-400'])}>
-          {time || 'TBD'}s
+        <span
+          className={clsx([metricStyle, cpm ? 'text-white' : 'text-slate-400'])}
+        >
+          {time ? time + 's' : 'TBD'}
         </span>
       </div>
     </div>
   )
 }
+
+const metricStyle = 'text-xl'
 
 export default Sidebar
