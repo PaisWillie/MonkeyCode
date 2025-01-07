a086926f6c7eb887ad86638adaccb18350601666
Willie Pai <PaisWillie@gmail.com>
2025-01-07 13:12:26 -0500

    feat: add leaderboard

---
+++ src/components/Leaderboard.tsx
@@ -0,0 +1,131 @@
+import { ConfigProvider, Table, theme } from 'antd'
+import clsx from 'clsx'
+import background from '../assets/background.jpg'
+
+interface DataType {
+  key: string
+  name: string
+  cpm: number
+  testType: string
+  accuracy: string
+  rawCpm: number
+}
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    sorter: (a: DataType, b: DataType) => a.name.localeCompare(b.name)
+  },
+  {
+    title: 'CPM',
+    dataIndex: 'cpm',
+    key: 'cpm',
+    sorter: (a: DataType, b: DataType) => a.cpm - b.cpm,
+    defaultSortOrder: 'descend' as const
+  },
+  {
+    title: 'Raw CPM',
+    dataIndex: 'rawCpm',
+    key: 'rawCpm',
+    sorter: (a: DataType, b: DataType) => a.rawCpm - b.rawCpm
+  },
+  {
+    title: 'Test type',
+    dataIndex: 'testType',
+    key: 'testType'
+  },
+  {
+    title: 'Accuracy',
+    dataIndex: 'accuracy',
+    key: 'accuracy',
+    sorter: (a: DataType, b: DataType) => a.accuracy.localeCompare(b.accuracy)
+  }
+]
+
+const data: DataType[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    cpm: 300,
+    testType: 'Standard',
+    accuracy: '95%',
+    rawCpm: 320
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    cpm: 280,
+    testType: 'Advanced',
+    accuracy: '92%',
+    rawCpm: 290
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    cpm: 320,
+    testType: 'Standard',
+    accuracy: '97%',
+    rawCpm: 340
+  }
+]
+
+const Leaderboard = () => {
+  return (
+    <ConfigProvider
+      theme={{
+        // 1. Use dark algorithm
+        algorithm: theme.darkAlgorithm,
+        components: {
+          Table: {
+            colorBgContainer: 'transparent',
+            borderColor: 'transparent'
+          }
+        }
+      }}
+    >
+      <div
+        className="flex h-screen w-screen flex-col items-center justify-between bg-cover bg-center font-['Space_Mono'] text-[#646669]"
+        style={{
+          backgroundImage: `url(${background})`
+        }}
+      >
+        <div className="grid size-full grid-rows-12 bg-black/15 p-8 backdrop-blur-2xl">
+          <header
+            className={clsx([
+              containerStyle,
+              'flex flex-row items-center justify-center text-xl text-white'
+            ])}
+          >
+            SnippetSprint Leaderboard
+          </header>
+          <main className="row-span-11 mt-8 size-full">
+            <div
+              id="code"
+              className={clsx([
+                containerStyle,
+                'flex size-full flex-row gap-x-4 p-2'
+              ])}
+            >
+              <Table<DataType>
+                columns={columns}
+                dataSource={data}
+                scroll={{ y: 400 }}
+                pagination={false} // Hide pagination
+                className="size-full"
+                rowClassName={(record, index) =>
+                  index === 0 ? 'text-yellow-500' : ''
+                }
+              />
+            </div>
+          </main>
+        </div>
+      </div>
+    </ConfigProvider>
+  )
+}
+
+const containerStyle = 'rounded-lg bg-slate-800/50'
+
+export default Leaderboard
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import 'tailwindcss/tailwind.css'
 import App from 'components/App'
+import Leaderboard from 'components/Leaderboard'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/leaderboard" element={<Leaderboard />} />
+    </Routes>
+  </BrowserRouter>
+)
